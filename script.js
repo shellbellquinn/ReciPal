7521632ab7acd49bd81c5970eb17552c6aef1292
Jacqueline Cameron <jskye429@mail.com>
2019-10-26 00:48:36 -0500

    having trouble with moment syntax in if statements for meal time

--- script.js
+++ script.js
@@ -1,5 +1,46 @@
+// moment meal suggestion 
+
+var hourNow = moment().format('MMMM Do YYYY, h:mm:ss a')
+
+var currentHour = moment().format('H')
+//breakfast lunch and dinner time
+
+if ('H'< "11").text("It's breakfast time!")
+    $(".subtitle").append(text)
+    console.log(text)
+    {
+    //  block of code to be executed if condition1 is true
+    } else if (condition2) {
+    //  block of code to be executed if the condition1 is false and condition2 is true
+    } else if (condition2) {
+
+    } else {
+    //  block of code to be executed if the condition1 is false and condition2 is false
+  }
+
+
+
+
+
+
 
 // click listener for meal time buttons
+// breakfast button
+$("#breakfastBox").click(function(){
+
+});
+
+// lunch button
+
+$("#lunchBox").click(function(){
+
+});
+// dinner button 
+
+$("#dinnerBox").click(function(){
+
+});
+
 
 //click listener for see more 
 
