4121c4542bf026cb6b0ad0fc721a37e4d0914540
Michelle Quinn <michelle.n.quinn@gmail.com>
2019-10-26 13:22:02 -0500

    foor loop to populate recipes

--- script.js
+++ script.js
@@ -1,65 +1,58 @@
-var mealType= "breakfast"
-var queryURL = "https://api.edamam.com/search?q=breakfast&app_id=$%7B12fc1523%7D&app_key=$%7B97aee21b6757a0b5b1eade0f194a5c24%7D&mealType=breakfast";
- 
-//  function defaultRecipe() {
+    var mealType = "breakfast"
+    //$(this).attr("data-attr");
+    var queryURL = "https://api.edamam.com/search?q=" + mealType + "&app_id=$%7B12fc1523%7D&app_key=$%7B97aee21b6757a0b5b1eade0f194a5c24%7D&mealType" + mealType;
 
-$.ajax({
-      url: queryURL,
-      method: "GET"
-    }).then(function(data) {
-
-    console.log(data);
-    console.log("test");
-
-  });
+    // Creates AJAX call for the specific movie button being clicked
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
 
 
+        for (var i = 0; i < 6; i++) {
+            var recipe = JSON.stringify(response.hits[i].recipe.label);
+            var imgSrc = JSON.stringify(response.hits[i].recipe.image);
 
-// click listener for meal time buttons
+            if (mealType === "breakfast") {
 
-//click listener for see more 
+                $('.columns').append(`<div class="column" data-attr="breakfast">
 
-// click listener 
-
-// search bar functionality utilizing API stuff 
-
-
-// click listener for recipie card pop out
-// $(".recipieAPI")
-
-
-
-
-
-// // FOR LOOP TO DISPLAY BREAKFAST LUNCH OR DINNER AFTER MOMENT
-  for (var i = 1; i < 12; i++) {
-  if (mealType = "breakfast"){
-
-      $('.columns').append(`<div class="column" data-attr="breakfast">
+            <!--Img-->
+                 <img class="recipeImg" src= ${imgSrc}>
     
              <!--text row-->
-                 <h2 class="recipeLabel"> RecipeLable </h2>
-    
-             <!--Img-->
-                 <img class="recipeImg" scr= imgSrc>`);
-  }
-  else if (mealType = "lunch"){
-      $('.columns').append(`<div class="column" data-attr="lunch">
-    
-             <!--text row-->
-                 <h2 class="recipeLabel"> RecipeLable </h2>
-    
-             <!--Img-->
-                 <img class="recipeImg" scr= imgSrc>`);
-  }
+                 <h2 class="recipeLabel">${recipe} </h2>
 
-  else if (mealType = "dinner"){
-      $('.columns').append(`<div class="column" data-attr="dinner">
+
+            <!--button row-->     
+            <button class="popUp"> View Recipe </button>`);
+
+
+            } else if (mealType === "lunch") {
+                $('.columns').append(`<div class="column" data-attr="lunch">
+
+            <!--Img-->
+                 <img class="recipeImg" src= ${imgSrc}>
     
              <!--text row-->
-                 <h2 class="recipeLabel"> RecipeLable </h2>
+                 <h2 class="recipeLabel">${recipe} </h2>
+
+
+            <!--button row-->     
+            <button class="popUp"> View Recipe </button>`);
+
+            } else {
+                $('.columns').append(`<div class="column" data-attr="dinner">
+
+            <!--Img-->
+                 <img class="recipeImg" src= ${imgSrc}>
     
-             <!--Img-->
-                 <img class="recipeImg" scr= imgSrc>`);
-  }
-  };
+             <!--text row-->
+                 <h2 class="recipeLabel">${recipe} </h2>
+
+
+            <!--button row-->     
+            <button class="popUp"> View Recipe </button>`);
+            }
+        }
+    })
