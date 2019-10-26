d53a9940976d77ab59dd698dfb00a8a7ac15d9d0
Jacqueline Cameron <jskye429@mail.com>
2019-10-26 13:06:34 -0500

    got moment stuff to work for breakfast, lunch and dinner

--- script.js
+++ script.js
@@ -1,22 +1,44 @@
 // moment meal suggestion 
+var zeroTest = 0
+var nineTime = moment().set("hour",9).format("H");
+var tenTime = moment().set("hour",10).format("H");
+var elevenTime = moment().set("hour",11).format("H");
+var twelveTime = moment().set("hour",12).format("H");
+var oneTime = moment().set("hour",13).format("H");
+var twoTime = moment().set("hour",14).format("H");
+var threeTime = moment().set("hour",15).format("H");
+var fourTime = moment().set("hour",16).format("H");
+var fiveTime = moment().set("hour",17).format("H");
+var sixTime = moment().set("hour", 18).format("H");
+var sevenTime = moment().set("hour", 19).format("H");
+var eightTime = moment().set("hour", 20).format("H");
 
-var hourNow = moment().format('MMMM Do YYYY, h:mm:ss a')
+//console.log(tenTime)
+
+var hourNow = moment().format('MMMM Do YYYY, HH:mm:ss a')
 
 var currentHour = moment().format('H')
+
 //breakfast lunch and dinner time
-
-if ('H'< "11").text("It's breakfast time!")
-    $(".subtitle").append(text)
-    console.log(text)
-    {
-    //  block of code to be executed if condition1 is true
-    } else if (condition2) {
-    //  block of code to be executed if the condition1 is false and condition2 is true
-    } else if (condition2) {
-
-    } else {
-    //  block of code to be executed if the condition1 is false and condition2 is false
-  }
+// if now > 0 && < 12
+console.log(currentHour)
+if (currentHour > 0 && currentHour < 11){
+    $(".subtitle").text("It's breakfast time!")
+    }
+//  block of code to be executed if condition1 is true
+else if (currentHour >= 11 && currentHour < 16) {
+  $(".subtitle").text("It's lunch time!");
+} else {
+  $(".subtitle").text("It's dinner time!");
+}
+    
+  
+//  block of code to be executed if the condition1 is false and condition2 is true
+// if (currentHour > 4 && currentHour < 8){
+ //  $(".subtitle").text("It's dinner time!")
+ //  }
+// else (currentHour > 8)
+// $(".subtitle").text("It's dinner or snack time!");
 
 
 
@@ -54,8 +76,3 @@ $(".recipieAPI")
 
 //this can be linked in the html
 
-<script src="https://code.jquery.com/jquery-3.4.1.min.js"
-integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.js"></script>
-
-<script src="script.js"></script>
