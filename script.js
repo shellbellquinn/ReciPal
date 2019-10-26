2ef3a0209300be37af8189cd9d898af04234a263
DwayneL <dr2@ameritech.net>
2019-10-26 12:24:20 -0500

    added code for modal on img click

--- script.js
+++ script.js
@@ -50,12 +50,9 @@ $("#dinnerBox").click(function(){
 
 
 // click listener for recipie card pop out
-$(".recipieAPI")
+var recipeModal = $("#recipeModal");
+$(".recipeImg").on("click", function(){
+    recipeModal.style.display = "block"  
+});
 
 //this can be linked in the html
-
-<script src="https://code.jquery.com/jquery-3.4.1.min.js"
-integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.js"></script>
-
-<script src="script.js"></script>
