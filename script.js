292c833eebb1cae58f8a7740e49e6b51a5dfcc85
Michelle Quinn <michelle.n.quinn@gmail.com>
2019-10-28 20:06:16 -0500

    merging changes to load more

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-var mealType 
-//$(this).attr("data-attr");
+var mealType
+var recipes
+var numToDisplay
 var queryURL = "https://api.edamam.com/search?q=" + mealType + "&app_id=$%7B12fc1523%7D&app_key=$%7B97aee21b6757a0b5b1eade0f194a5c24%7D&mealType" + mealType;
 
 // Creates AJAX call for the specific movie button being clicked
@@ -7,11 +8,20 @@ $.ajax({
     url: queryURL,
     method: "GET"
 }).then(function (response) {
+   recipes = response;
+   displayRecipes(6)
+});
 
+$('#seeMore').on('click', function() {
+    displayRecipes(12)
+  });
 
-    for (var i = 0; i < 6; i++) {
-        var recipe = JSON.stringify(response.hits[i].recipe.label);
-        var imgSrc = JSON.stringify(response.hits[i].recipe.image);
+function displayRecipes(num) {
+    numToDisplay += num 
+    $( ".columns" ).empty();
+    for (var i = 0; i < num; i++) {
+        var recipe = JSON.stringify(recipes.hits[i].recipe.label);
+        var imgSrc = JSON.stringify(recipes.hits[i].recipe.image);
 
         if (mealType === "breakfast") {
 
@@ -24,8 +34,6 @@ $.ajax({
 
         <!--button row-->     
             <button class="popUp"> View Recipe </button>`);
-
-      
 
 
         } else if (mealType === "lunch") {
@@ -49,82 +57,4 @@ $.ajax({
         <button class="popUp"> View Recipe </button>`);
         }
     }
-
-
-})
-
-
-
- // moment meal suggestion 
- var zeroTest = 0
- var nineTime = moment().set("hour", 9).format("H");
- var tenTime = moment().set("hour", 10).format("H");
- var elevenTime = moment().set("hour", 11).format("H");
- var twelveTime = moment().set("hour", 12).format("H");
- var oneTime = moment().set("hour", 13).format("H");
- var twoTime = moment().set("hour", 14).format("H");
- var threeTime = moment().set("hour", 15).format("H");
- var fourTime = moment().set("hour", 16).format("H");
- var fiveTime = moment().set("hour", 17).format("H");
- var sixTime = moment().set("hour", 18).format("H");
- var sevenTime = moment().set("hour", 19).format("H");
- var eightTime = moment().set("hour", 20).format("H");
-
-// //console.log(tenTime)
-
- var hourNow = moment().format('MMMM Do YYYY, HH:mm:ss a')
-
- var currentHour = moment().format('H')
-
- //breakfast lunch and dinner time
- // if now > 0 && < 12
-//console.log(currentHour)
-
-
-
-if (currentHour > 0 && currentHour < 11) {
-    $(".subtitle").text("It's breakfast time!")
-    mealType === "breakfast"
 }
-//  block of code to be executed if condition1 is true
-else if (currentHour >= 11 && currentHour < 16) {
-    $(".subtitle").text("It's lunch time!");
-    mealType === "lunch"
-} else {
-    $(".subtitle").text("It's dinner time!");
-    mealType = "dinner"
-}
-
-
-//  block of code to be executed if the condition1 is false and condition2 is true
-// if (currentHour > 4 && currentHour < 8){
-//  $(".subtitle").text("It's dinner time!")
-//  }
-// else (currentHour > 8)
-// $(".subtitle").text("It's dinner or snack time!");
-
-
-
-
-
-//click listener for meal time buttons
-//breakfast button
-// $("#breakfastBox").click(function () {
-
-// });
-
-// // lunch button
-
-// $("#lunchBox").click(function () {
-
-// });
-// // dinner button 
-
-// $("#dinnerBox").click(function () {
-
-// });
-
-
-// //click listener for see more 
-
-// click listener 
